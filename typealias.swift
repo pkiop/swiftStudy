2fd588a442ab8e0d992254662535ce317592e2fd
pkiop <vivezdv@gmail.com>
2020-06-04 23:04:29 +0900

    code

---
+++ typealias.swift
@@ -0,0 +1,10 @@
+
+typealias SampleRate = UInt8 //typedef
+typealias GridPoint = (Int, Int)
+
+var touchPoint: GridPoint = GridPoint(10, 15)
+
+typealias GP = (x: Int, y: Int)
+var tP: GP = GP(7, 2)
+
+print(tP.x)
